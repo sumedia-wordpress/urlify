b5533140a46bd12b92c7cce2f5a1a8cee78b2524
Sven Ullmann <kontakt@sumedia-webdesign.de>
2019-12-09 02:37:03 +0100

    combine wp repo and composer/github

--- sumedia-base.php
+++ sumedia-base.php
@@ -4,5 +4,10 @@ if (!defined('SUMEDIA_BASE_VERSION')) {
     define('SUMEDIA_PLUGIN_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
     define('SUMEDIA_PLUGIN_URL', plugin_dir_url(__DIR__));
 
+    define('SUMEDIA_BASE_INLCUDING_PLUGIN', basename(__DIR__));
+
+    define('SUMEDIA_BASE_PATH', dirname(__FILE__) . str_replace('/', DIRECTORY_SEPARATOR, '/vendor/sumedia-wordpress/base'));
+    define('SUMEDIA_BASE_URL', SUMEDIA_PLUGIN_URL . SUMEDIA_BASE_INLCUDING_PLUGIN . '/vendor/sumedia-wordpress/base');
+
     require_once(__DIR__ . str_replace('/', DIRECTORY_SEPARATOR, '/vendor/sumedia-wordpress/base/sumedia-base.php'));
 }
